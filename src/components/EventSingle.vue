bdb981c5b3077ead58a1ad0935992f2a863c146a
Peter Lollo <peterlollo@Peters-MacBook-Pro.local>
2018-02-09 16:49:40 -0800

    Render single event

---
+++ src/components/EventSingle.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id='single-event'>
+    <h4>Single Event</h4>
+    <div class='event-row'>
+      <div>description: </div>
+      <div>{{ event.description }}</div>
+    </div>
+    <div class='event-row'>
+      <div>location: </div>
+      <div>{{ event.location }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [ 'event' ]
+}
+</script>
+
+<style scoped>
+#single-event {
+  display: flex;
+  flex-direction: column;
+}
+.event-row {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,98 +1,25 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Events</h2>
-    <div v-if='events.length'>{{ events }}</div>
-    <div v-else>Pardon us, there's an error somewhere. Please try again.</div>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <div id='events-list'>
+      <h2>Events</h2>
+      <div v-if='events.length'>
+        <EventSingle :event='events[0]'/>
+      </div>
+      <div v-else>Pardon us, there's an error somewhere. Please try again.</div>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
+import EventSingle from './EventSingle'
 
 export default {
   name: 'HelloWorld',
   methods: mapActions([ 'getEvents' ]),
   computed: mapGetters([ 'events' ]),
+  components: { EventSingle },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'

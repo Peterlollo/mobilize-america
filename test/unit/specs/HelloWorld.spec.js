4b8942eab25f7aab09ede21812cb51a6f3e43004
Peter Lollo <peterlollo@Peters-MacBook-Pro.local>
2018-02-12 17:45:37 -0800

    Fix tests

--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -1,11 +1,7 @@
-import Vue from 'vue'
-import HelloWorld from '@/components/HelloWorld'
+import Events from '@/components/Events'
 
-describe('HelloWorld.vue', () => {
-  it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .toEqual('Welcome to Your Vue.js App')
+describe('Events', () => {
+  it('has a created hook', () => {
+    expect(typeof Events.created).toBe('function')
   })
 })
